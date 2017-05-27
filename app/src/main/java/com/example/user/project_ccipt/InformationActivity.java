1d6bb7c4d91c822ca02ea0a95f58a23aa28518f9
Jun-seon <hjs0579@ajou.ac.kr>
2017-05-27 17:36:57 +0900

    chatting update

--- app/src/main/java/com/example/user/project_ccipt/InformationActivity.java
+++ app/src/main/java/com/example/user/project_ccipt/InformationActivity.java
@@ -1,40 +1,21 @@
 package com.example.user.project_ccipt;
 
-import android.app.Activity;
 import android.content.Intent;
 import android.location.Address;
 import android.location.Geocoder;
 import android.net.Uri;
 import android.os.Bundle;
-import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
-import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
-import com.bumptech.glide.Glide;
-import com.google.android.gms.auth.api.Auth;
-import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
-import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
-import com.google.android.gms.auth.api.signin.GoogleSignInResult;
-import com.google.android.gms.common.ConnectionResult;
-import com.google.android.gms.common.SignInButton;
-import com.google.android.gms.common.api.GoogleApiClient;
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
-import com.google.firebase.auth.AuthCredential;
-import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
-import com.google.firebase.auth.GoogleAuthProvider;
 import com.google.firebase.auth.UserInfo;
-import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
-import java.io.IOException;
-import java.util.List;
 
 /**
  * Created by user on 2017-04-26.
@@ -44,7 +25,6 @@ public class InformationActivity extends AppCompatActivity implements View.OnCli
 
     private static final String TAG = "InformationActivity";
 
-    final Geocoder geocoder = new Geocoder(this);
 
     // Write a message to the database
     FirebaseDatabase database = FirebaseDatabase.getInstance();
@@ -98,30 +78,6 @@ public class InformationActivity extends AppCompatActivity implements View.OnCli
     public void onClick(View v) {
         switch (v.getId()) {
             case R.id.submit_btn: {
-
-                List<Address> list = null;
-
-
-                String str = user_location.getText().toString();
-                try {
-                    list = geocoder.getFromLocationName(
-                            str, // 지역 이름
-                            10); // 읽을 개수
-                } catch (IOException e) {
-                    e.printStackTrace();
-                    Log.e("test","입출력 오류 - 서버에서 주소변환시 에러발생");
-                }
-
-                if (list != null) {
-                    if (list.size() == 0) {
-                        Toast.makeText(getApplicationContext(),"해당되는 주소 정보는 없습니다",Toast.LENGTH_LONG).show();
-                    } else {
-                        Toast.makeText(getApplicationContext(),list.get(0).toString(),Toast.LENGTH_LONG).show();
-                        //          list.get(0).getCountryName();  // 국가명
-                        //          list.get(0).getLatitude();        // 위도
-                        //          list.get(0).getLongitude();    // 경도
-                    }
-                }
 
                 writeNewUser(userName, userUid, userPhotoUrl);
 
--- app/src/main/java/com/example/user/project_ccipt/TeamChatActivity.java
+++ app/src/main/java/com/example/user/project_ccipt/TeamChatActivity.java
@@ -2,14 +2,36 @@ package com.example.user.project_ccipt;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.support.annotation.Nullable;
+import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
 
 /**
  * Created by user on 2017-04-26.
@@ -18,13 +40,25 @@ import com.google.firebase.auth.FirebaseUser;
 public class TeamChatActivity extends Activity {
 
     private static final String currentTeamName = TeamListActivity.currentTeamName;
+    private static final String TAG = "TeamChatActivity";
     Team currentTeam = new Team(currentTeamName);
 
-    Button brainstorming_button,appointment_button,teamchat_button,teammember_button;
+    Button brainstorming_button,appointment_button,teamchat_button,teammember_button,send_button;
+    EditText send_message;
 
     private FirebaseAuth mFirebaseAuth;
     private FirebaseUser mFirebaseUser;
 
+    FirebaseUser currentUser = FirebaseAuth.getInstance().getCurrentUser();
+
+    FirebaseDatabase database = FirebaseDatabase.getInstance();
+    DatabaseReference chatRef = database.getReference("Teams").child(currentTeamName).child("messages");
+
+    ListView chatList;
+    ArrayList<String> messages = new ArrayList<>();
+    ArrayList<String> times = new ArrayList<>();
+    ArrayList<String> names = new ArrayList<>();
+    ArrayList<Bitmap> photos = new ArrayList<>();
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -32,12 +66,14 @@ public class TeamChatActivity extends Activity {
         setContentView(R.layout.teamchat);
 
         TextView teamNameTextView = (TextView) findViewById(R.id.TeamChatTeamName);
-        teamNameTextView.setText(currentTeam.getTitle());
+        teamNameTextView.setText(currentTeamName);
 
         brainstorming_button=(Button)findViewById(R.id.brainstorming_button);
         appointment_button=(Button)findViewById(R.id.appointment_button);
         teamchat_button=(Button)findViewById(R.id.teamchat_button);
         teammember_button=(Button)findViewById(R.id.teammember_button);
+        send_button=(Button)findViewById(R.id.send_button);
+        send_message=(EditText)findViewById(R.id.send_message);
 
         brainstorming_button.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -70,6 +106,100 @@ public class TeamChatActivity extends Activity {
                 startActivity(newIntent);
             }
         });
+        send_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                // 현재시간을 msec 으로 구한다.
+                long now = System.currentTimeMillis();
+                // 현재시간을 date 변수에 저장한다.
+                Date date = new Date(now);
+                // 시간을 나타냇 포맷을 정한다 ( yyyy/MM/dd 같은 형태로 변형 가능 )
+                SimpleDateFormat sdfNow = new SimpleDateFormat("HH:mm:ss");
+                // nowDate 변수에 값을 저장한다.
+                String formatDate = sdfNow.format(date);
+
+                //공백처리
+                if(send_message.getText().toString().trim().length() != 0){
+                    writeNewMember(send_message.getText().toString().trim(), formatDate);
+                }
+
+                send_message.setText("");
+            }
+        });
+
+        chatRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+
+                Iterable<DataSnapshot> contactChildren = dataSnapshot.getChildren();
+
+                messages.clear();
+                times.clear();
+                names.clear();
+                photos.clear();
+                for (DataSnapshot contact : contactChildren) {
+
+                    Uri imageUri = Uri.parse(contact.child("chatPhoto").getValue().toString());
+
+                    Bitmap bitmap = null;
+                    //TODO: exception error
+                    try {
+                        bitmap = MediaStore.Images.Media.getBitmap(getContentResolver(), imageUri);
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                    photos.add(bitmap);
+                    names.add(contact.child("chatName").getValue().toString());
+                    times.add(contact.child("chatTime").getValue().toString());
+                    messages.add(contact.child("chatText").getValue().toString());
+
+                }
+
+                makeCustomList();
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+                Log.w(TAG, "loadPost:onCancelled", databaseError.toException());
+            }
+        });
+    }
+
+    public void makeCustomList() {
+        CustomList adapter = new CustomList(this);
+        chatList = (ListView)findViewById(R.id.chatListView);
+        chatList.setAdapter(adapter);
+
+        chatList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Toast.makeText(getBaseContext(), messages.get(+position), Toast.LENGTH_LONG).show();
+            }
+        });
+    }
+
+    public class CustomList extends ArrayAdapter<String> {
+        private final Activity context;
+        public CustomList(Activity context) {
+            super(context, R.layout.chatlistview_other, messages);
+            this.context = context;
+        }
+
+        @Override
+        public View getView(int position, View view, ViewGroup parent) {
+            LayoutInflater inflater = context.getLayoutInflater();
+            View rowView = inflater.inflate(R.layout.chatlistview_other, null, true);
+            TextView chatName = (TextView) rowView.findViewById(R.id.chatName);
+            TextView chatText = (TextView) rowView.findViewById(R.id.chatText);
+            TextView chatTime = (TextView) rowView.findViewById(R.id.chatTime);
+            ImageView chatImage = (ImageView) rowView.findViewById(R.id.chatImage);
+            chatText.setText(messages.get(+position));
+            chatTime.setText(times.get(+position));
+            chatName.setText(names.get(+position));
+            chatImage.setImageBitmap(photos.get(+position));
+            return rowView;
+        }
     }
 
     private static class Team {
@@ -87,5 +217,38 @@ public class TeamChatActivity extends Activity {
         public void setTitle(String currentTeam) {
             teamTitle = currentTeam;
         }
+    }
+
+
+    private static class Chat {
+        Chat() {};
+        Chat(String name, String photo, String text, String time) {
+            chatName = name;
+            chatPhoto = photo;
+            chatText = text;
+            chatTime = time;
+        };
+
+        //TODO : Developing userAddress
+        private String chatName, chatPhoto, chatText, chatTime;
+
+        public String getChatName() {
+            return chatName;
+        };
+        public String getChatPhoto() {
+            return chatPhoto;
+        }
+        public String getChatText() {
+            return chatText;
+        };
+        public String getChatTime() {
+            return chatTime;
+        }
+    }
+
+    private void writeNewMember(String text, String time) {
+        Chat chat = new Chat(currentUser.getDisplayName(), currentUser.getPhotoUrl().toString(), text, time);
+
+        chatRef.push().setValue(chat);
     }
 }
